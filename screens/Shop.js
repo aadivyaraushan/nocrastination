0e02c21c8cfbc2835cb42acdfe0df1610c8c8ef0
aadivyaraushan <96186374+aadivyaraushan@users.noreply.github.com>
2022-04-11 11:41:42 +0400

    Removed Shop.js

--- screens/Shop.js
+++ screens/Shop.js
@@ -1,57 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import Topbar from 'components/Topbar.js'
-import BottomBar from 'components/BottomBar.js'
-import {auth, db} from 'firebase.js';
-import { collection, getDocs } from "firebase/firestore"; 
-import { assertYieldExpression } from "babel-types";
-/*
-1) Shop Component (done)
-2) Setting each item in scrollable flex-box with 2 in each row
-*/
-const querySnapshot = async () => {await getDocs(collection(db, "ShopItems"))};
-var items = []
-const Item = (props) => {
+
+export default function Shop() {
   return (
-
-    <View style={styles.itemDiv}>
-      <Image source={require(props.image)}>
-      </Image>
-      <Text>{props.name}</Text>
-      <Text>{props.price}</Text>
-
+    <View>
+      <Text>Shop</Text>
     </View>
   );
 }
-querySnapshot.forEach((doc) => {
-  items.push(
-  {"name":doc.data()["name"],
-  "image":doc.data()["image"],
-  "price":doc.data()["price"]})
-})
-const Shop = () => {
-  return (
-    <View style = {{flexDirection : "row", flexWrap : "wrap", backgroudnColor : "black"}}>
-      <Topbar/>
-      //needs code for positioning
-      {items.map(i=>{
-        return <Item name = {i["name"]} image = {i["image"]} price = {i["price"]} style = {styles.it}></Item>
-      })}//to render each item
-      <BottomBar/>
-    </View>
-  );
-};
 
-export default Shop;
-
-const styles = StyleSheet.create({
-  itemDiv : {
-    backgroundColor : 'turquoiuse',
-    alignContent : 'center',
-    justifyContent : 'center'
-  },
-  it : {
-    flex : 1,
-    flexShrink : 1
-  }
-});
+const styles = StyleSheet.create({});
