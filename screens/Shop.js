9b5de4d3567f15bbba3a3753c33e68d210cf80fe
NinjaWarrior <66741195+superman-tech@users.noreply.github.com>
2022-04-16 07:25:00 +0400

    Update Shop.js
    changes made, just needs backend working

--- screens/Shop.js
+++ screens/Shop.js
@@ -11,7 +11,10 @@ const Shop = ()=>{
   const [loaded] = useFonts({
     e: require('../assets/fonts/RetroGaming-Regular.ttf'),
   });
-  
+  const qt = async () => await getDocs(collection(db, "users")).then(function(val){
+    return val
+  });
+  console.log(qt);
   if (!loaded) {
     return null;
   }
@@ -29,34 +32,24 @@ const Shop = ()=>{
       "image": require("../assets/ruby.png"),
       "price": "30 coins",
       "level": "Level : 10",
-      },
-      {
-        "name": "carrot",
-        "image": require("../assets/ruby.png"),
-        "price": "30 coins",
-        "level": "Level : 10",
-        },
-        {
-          "name": "carrot",
-          "image": require("../assets/ruby.png"),
-          "price": "30 coins",
-          "level": "Level : 10",
-          },
-          {
-            "name": "carrot",
-            "image": require("../assets/ruby.png"),
-            "price": "30 coins",
-            "level": "Level : 10",
-            },
-            {
-              "name": "carrot",
-              "image": require("../assets/ruby.png"),
-              "price": "30 coins",
-              "level": "Level : 10",
-              },
+      }
     ];
+    const q2 = [{
+      "theme": "galatica",
+    "image": require("../assets/coin.png"),
+    "price": "77 coins",
+    "level": "Level : 3",
+    },
+    {
+      "theme": "mogus",
+    "image": require("../assets/coin.png"),
+    "price": "770 coins",
+    "level": "Level : 30",
+    },
+  ]
     var i = 0;
   var items = []
+  var themes = []
   querySnapshot.forEach(doc => {
     items.push(
     {"name":doc["name"],
@@ -64,16 +57,63 @@ const Shop = ()=>{
     "price":doc["price"],
   "level" : doc["level"]})
   })
+  q2.forEach(doc => {
+    themes.push(
+      {"theme":doc["theme"],
+      "image":doc["image"],
+      "price":doc["price"],
+      "level":doc["level"]}
+    )
+  })
     return (
-      <View style = {{flexDirection : "row", flexWrap:"wrap", flex : 1}}>
-        <ImageBackground source = {require("../assets/backgroud.png")}>
-          <Topbar/>
-      <ScrollView style={{ flex: 1 }}
-        horizontal={true}
-        nestedScrollEnabled={true}
+      <View style = {{ flexWrap:"wrap", flex : 1,backgroundColor : "black"}}>
+      <Topbar/>
+        <ScrollView 
+        
+        
         contentContainerStyle={{
-          flex: 1,
+          
           flexWrap: "wrap",
+        }}>
+      <View style = {{flexWrap:"wrap", flex : 1, height : '50%', width : '100%', padding : 20}}>
+      <Text style = {{fontFamily : 'e', fontSize : 100, color:"white", flex : 1}}>Themes</Text>
+      <ScrollView style={{padding : 1}}
+
+          horizontal = {2}
+        contentContainerStyle={{
+          flexWrap: "wrap",
+          flex : 1,
+          flexDirection : "row"
+        }}>
+       {themes.map((a) => {
+          return(
+            <Pressable style={styles.itemDiv} onpress={()=>
+            {/*add funtionality for reducing points (get access to db for user and then reduce points,
+              if points are not enough, alert user using alert() function)*/
+            }}> 
+          <View key = {Math.random()}>
+            <Image key = {0} source = {a["image"]}></Image>
+          <Text key = {1} style={styles.txt1}>{a["theme"]}</Text>
+          <Text key = {2} style={styles.txt2}>{a["price"]}</Text> 
+          <Text key = {3} style={styles.txt3}>{a["level"]}</Text>
+          </View>
+          </Pressable>
+          );
+          
+        }
+          )
+          }
+          </ScrollView>
+          </View>
+          <View style = {{flexWrap:"wrap", flex : 1, height : '50%', width : '100%', padding : 20}}>
+          <Text style = {{fontFamily : 'e', fontSize : 100, color:"white", flex : 1}}>Items</Text>
+      <ScrollView style={{padding : 2}}
+
+          horizontal = {2}
+        contentContainerStyle={{
+          flexWrap: "wrap",
+          flex : 1,
+          flexDirection : "row"
         }}>
        {items.map((a) => {
           return(
@@ -94,8 +134,10 @@ const Shop = ()=>{
           )
           }
           </ScrollView>
+         </View>
+         </ScrollView>
       <BottomBar/>
-      </ImageBackground>
+      
         
       </View>
   );
@@ -105,22 +147,17 @@ const styles = StyleSheet.create({
   itemDiv : {
 
     backgroundColor : '#30D5C8',
-    width : 450,
-    height : 350,
+    width : 200,
+    height : 300,
     alignItems : "center",
     justifyContent : 'space-around',
-    margin : 30,
-
-    
+    margin : 50,
+    flexShrink : 1,
+  
     fontFamily : 'e'
     
   },
-  it : {
-    flexShrink : 1,
-    width : "200",
-    height : "300"
-
-  },
+  
   txt1 : {
     fontFamily : 'e',
     fontSize  : 30
