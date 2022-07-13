e4597e9473972b25797105a7e459aa74f1fc8d17
NinjaWarrior <66741195+superman-tech@users.noreply.github.com>
2022-07-13 11:32:17 +0400

    Update TaskSelect.js

--- screens/TaskSelect.js
+++ screens/TaskSelect.js
@@ -5,6 +5,7 @@ import {
   Text,
   View,
   Pressable,
+  Dimensions
 } from "react-native";
 import React from "react";
 import Topbar from "../components/Topbar";
@@ -71,13 +72,14 @@ const PickATask = ({ navigation }) => {
     </View>
   );
 };
-
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 export default PickATask;
 
 const styles = StyleSheet.create({
   bg: {
-    width: "100%",
-    height: "100%",
+    width: windowWidth,
+    height: windowHeight,
     resizeMode: "contain",
     display: "flex",
     alignItems: "center",
